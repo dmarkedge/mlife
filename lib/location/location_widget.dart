4db6af1deabacfb92b7c4895ea4c9f3bacb65ae4
dmarkedge <81028897+dmarkedge@users.noreply.github.com>
2021-05-19 21:12:01 -0600

    Update location_widget.dart

--- lib/location/location_widget.dart
+++ lib/location/location_widget.dart
@@ -1,21 +1,38 @@
-import '../flutter_flow/flutter_flow_theme.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class LocationWidget extends StatefulWidget {
-  LocationWidget({Key key}) : super(key: key);
+void main() => runApp(MyApp());
 
+class MyApp extends StatefulWidget {
   @override
-  _LocationWidgetState createState() => _LocationWidgetState();
+  _MyAppState createState() => _MyAppState();
 }
 
-class _LocationWidgetState extends State<LocationWidget> {
-  final scaffoldKey = GlobalKey<ScaffoldState>();
+class _MyAppState extends State<MyApp> {
+  late GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(45.521563, -122.677433);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      key: scaffoldKey,
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Maps Sample App'),
+          backgroundColor: Colors.green[700],
+        ),
+        body: GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(
+            target: _center,
+            zoom: 11.0,
+          ),
+        ),
+      ),
     );
   }
 }
